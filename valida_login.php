c4706c943611a1b8c120eab908978e13c4f83e41
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-06-28 12:27:02 -0300

    First commit

---
+++ valida_login.php
@@ -0,0 +1,56 @@
+<?php
+
+  session_start();
+
+  //variável que verifica se a autenticação foi realizada
+  $usuario_autenticado = false;
+
+  //usuarios do sistema
+  $usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+  );
+
+  /*
+  echo '<pre>';
+  print_r($usuarios_app);
+  echo*/
+
+  foreach($usuarios_app as $user) {
+
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+      $usuario_autenticado = true;
+    }
+  }
+
+  if($usuario_autenticado) {
+    echo 'Usuário autenticado';
+    $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['x'] = 'um valor';
+    $_SESSION['y'] = 'outro valor';
+    header('Location: home.php');
+  } else {
+    $_SESSION['autenticado'] = 'NÃO';
+    header('Location: index.php?login=erro');
+  }
+
+/*
+print_r($_GET);
+
+echo '<br>';
+
+echo $_GET['email'];
+echo '<br>';
+echo $_GET['senha'];
+
+print_r($_POST);
+
+echo '<br>';
+
+echo $_POST['email'];
+echo '<br>';
+echo $_POST['senha'];
+*/
+
+
+?>
