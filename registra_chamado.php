e7a652de7efa4f6706a7aedc7411d821fcfdc145
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-06-28 12:56:23 -0300

    Criada a página 'registrar chamados'

---
+++ registra_chamado.php
@@ -0,0 +1,27 @@
+<?php
+
+  echo '<pre>';
+  print_r($_POST);
+  echo '</pre>';
+
+  //estamos trabalhando na montagem do texto
+  $titulo = str_replace('#', '-', $_POST['titulo']);
+  $categoria = str_replace('#', '-', $_POST['categoria']);
+  $descricao = str_replace('#', '-', $_POST['descricao']);
+
+  //implode('#', $_POST);
+
+  $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+
+  //abrindo o arquivo e escrevendo o texto
+  $arquivo = fopen('arquivo.hd', 'a');
+
+  fwrite($arquivo, $texto);
+
+  //fechando o arquivo
+  fclose($arquivo);
+
+  header('Location: abrir_chamado.php');
+
+?>
